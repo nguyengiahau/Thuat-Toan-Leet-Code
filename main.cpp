81e0b19b796a152a8de6cc854aa61649b19aad0a
nguyengiahau <2224802010349@student.tdmu.edu.vn>
2023-07-06 20:37:46 +0700

    Beginner Free Contest 53 - DELIVERY

--- main.cpp
+++ main.cpp
@@ -1,59 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <iomanip>
+#include <math.h>
+#include <algorithm>
 using namespace std;
-void printVector(vector<int> &v)
-{
-    for (int i = 0; i < v.size(); i++)
-        cout << v[i] << " ";
-    cout << endl;
-}
-vector<int> megreSort(vector<int> &nums1, vector<int> &nums2)
-{
-    vector<int> res;
-    int n = nums1.size();
-    int m = nums2.size();
-    int i = 0, j = 0;
-    while (i < n && j < m)
-    {
-        if (nums1[i] < nums2[j])
-            res.push_back(nums1[i]), i++;
-        else
-            res.push_back(nums2[j]), j++;
-    }
-    while (i < n)
-    {
-        res.push_back(nums1[i]);
-        i++;
-    }
-    while (j < m)
-    {
-        res.push_back(nums2[j]);
-        j++;
-    }
-    return res;
-}
-double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
-{
-    vector<int> temp = megreSort(nums1, nums2);
-    printVector(temp);
-    int n = temp.size();
-    if (n % 2 != 0)
-        return temp[(n - 1) / 2];
-    return 1.0 * (temp[n / 2] + temp[(n / 2) - 1]) / 2;
-}
 int main()
 {
-    // cout << "Hello world" << endl;
-
-    int n, m;
-    cin >> n;
-    vector<int> a(n, 0);
+    long long n, m, ans = 0;
+    cin >> n >> m;
+    long long *arr = new long long[n];
     for (int i = 0; i < n; i++)
-        cin >> a[i];
-    cin >> m;
-    vector<int> b(m, 0);
-    for (int i = 0; i < m; i++)
-        cin >> b[i];
-    cout << fixed << setprecision(5) << findMedianSortedArrays(a, b);
+        cin >> arr[i];
+    sort(arr, arr + n);
+    long long i = 0;
+    while (arr[i] < 0 && i < n)
+    {
+        ans += abs(arr[i]) * 2;
+        i += m;
+    }
+    i = n - 1;
+    while (arr[i] > 0 && i > -1)
+    {
+        ans += abs(arr[i]) * 2;
+        i -= m;
+    }
+    cout << ans;
 }
